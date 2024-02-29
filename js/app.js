c5b35cdb211be934edfdf9fe30efa40d6db02d4a
DKH <derek.hawryluk@edu.sait.ca>
2024-02-29 14:47:24 -0700

    search year added

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var searchInput = document.getElementById('searchInput');
-  let searchButton = document.getElementById('searchButton');
-  const searchResults = document.getElementById('searchResults');
+document.addEventListener("DOMContentLoaded", function () {
+  var searchInput = document.getElementById("searchInput");
+  let searchButton = document.getElementById("searchButton");
+  const searchResults = document.getElementById("searchResults");
+  const selectElement = document.getElementById("searchType");
 
-  searchButton.addEventListener('click', function() {
-      const searchTerm = searchInput.value.trim();
-      const results = searchBooksByTitle(searchTerm);
-      displaySearchResults(results);
+  selectElement.addEventListener("change", (e) => {
+    const searchYearResults = searchBookByYear(e.target.value);
+    displaySearchResults(searchYearResults);
+  });
+
+  searchButton.addEventListener("click", function () {
+    const searchTerm = searchInput.value.trim();
+    const results = searchBooksByTitle(searchTerm);
+    displaySearchResults(results);
   });
 
   function displaySearchResults(results) {
-      var output = '';
-      results.forEach(book => {
-          output += `
+    var output = "";
+    results.forEach((book) => {
+      output += `
               <div class="book-item">
                   <h3>${book.title}</h3>
                   <p><strong>Author:</strong> ${book.author}</p>
                   <p><strong>Year:</strong> ${book.year}</p>
               </div>
           `;
-      });
-      searchResults.innerHTML = output;
+    });
+    searchResults.innerHTML = output;
   }
 });
