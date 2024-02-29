f030f3085c1988d3952360d2ebd12fd3157faeb9
Brittany-Maraneta <Brittany-june@hotmail.com>
2024-02-29 14:48:47 -0700

    view all pg

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var searchInput = document.getElementById('searchInput');
-  let searchButton = document.getElementById('searchButton');
-  const searchResults = document.getElementById('searchResults');
+document.addEventListener("DOMContentLoaded", function () {
+  var searchInput = document.getElementById("searchInput");
+  let searchButton = document.getElementById("searchButton");
+  const searchResults = document.getElementById("searchResults");
 
-  searchButton.addEventListener('click', function() {
-      const searchTerm = searchInput.value.trim();
-      const results = searchBooksByTitle(searchTerm);
-      displaySearchResults(results);
+  searchButton.addEventListener("click", function () {
+    const searchTerm = searchInput.value.trim();
+    const results = searchBooksByTitle(searchTerm);
+    displaySearchResults(results);
   });
 
   function displaySearchResults(results) {
-      var output = '';
-      results.forEach(book => {
-          output += `
+    var output = "";
+    results.forEach((book) => {
+      output += `
               <div class="book-item">
                   <h3>${book.title}</h3>
                   <p><strong>Author:</strong> ${book.author}</p>
                   <p><strong>Year:</strong> ${book.year}</p>
               </div>
           `;
-      });
-      searchResults.innerHTML = output;
+    });
+    searchResults.innerHTML = output;
   }
 });
+
+//creating a view all pg with a button
+function myFunction() {
+  location.replace("/view.html");
+  console.log("is this working");
+}
