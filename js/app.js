8e8d5a5857da8ebd923dd50fad9a442e87d4747c
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-03-01 16:43:04 -0700

    functions for show more button

--- js/app.js
+++ js/app.js
@@ -10,16 +10,36 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   function displaySearchResults(results) {
-      var output = '';
+      let output = '';
       results.forEach(book => {
           output += `
               <div class="book-item">
                   <h3>${book.title}</h3>
                   <p><strong>Author:</strong> ${book.author}</p>
                   <p><strong>Year:</strong> ${book.year}</p>
-              </div>
-          `;
+
+                  <p id="showMoreText"><strong>Description:</strong> ${book.description}</p>
+                  <div class="button">
+                    <button type="button" id="show-toggle-btn" onclick="moreBtn()">Show More</button>
+                  </div>
+          </div>
+      `
       });
       searchResults.innerHTML = output;
-  }
+
+    }
 });
+
+
+function moreBtn() {
+    const text = document.getElementById("showMoreText");
+    const buttonTxt = document.getElementById("show-toggle-button");
+
+    if (text.style.display == "none") {
+        text.style.display = "inline";
+        buttonTxt.textContent = "Show More";
+            } else {
+        text.style.display = "none";
+        buttonTxt.textContent = "Show Less";
+    }
+}
--- js/bookstore.js
+++ js/bookstore.js
@@ -1,10 +1,10 @@
 // Sample database of books
  const books = [
-  { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925, image:"<img" },
-  { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  { id: 3, title: "1984", author: "George Orwell", year: 1949 },
-  { id: 4, title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951 },
-  { id: 5, title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+  { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+  { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+  { id: 3, title: "1984", author: "George Orwell", year: 1949, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+  { id: 4, title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+  { id: 5, title: "Pride and Prejudice", author: "Jane Austen", year: 1813, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
 ];
 
 console.log(books);
@@ -13,3 +13,4 @@ function searchBooksByTitle(title) {
   const result = books.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
   return result.length > 0 ? result : [{ title: "No books found with that title." }];
 }
+
